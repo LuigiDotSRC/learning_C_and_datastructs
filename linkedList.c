9828a8b0bbe40935da652285f5f03e6eb4d5891c
Luigi <129404345+LuigiDotSRC@users.noreply.github.com>
2023-07-20 10:29:36 -0400

    Update linkedList.c

--- linkedList.c
+++ linkedList.c
@@ -23,6 +23,29 @@ int count_of_nodes(struct node* head){
     return count; 
 }
 
+void insert_node(struct node* current_node, int node_data){
+    // insert node directly to the right of the current node: 
+    // current node link must be updated to match new node 
+    // the address of the next node must be stored inside the new node 
+    
+    struct node* new_node = NULL;
+    new_node = (struct node*)malloc(sizeof(struct node));
+    new_node->link = current_node->link; 
+    current_node->link = new_node; 
+    
+    new_node->data = node_data; 
+}
+
+struct node* traverse_nodes(struct node* head, int index){
+    struct node* traveller = head; 
+    for(int i=0; i<index; i++){
+        if(traveller->link != NULL){
+            traveller = traveller->link;
+        }
+    }
+    return traveller;
+}
+
 int main()
 {
     struct node* head = NULL; 
@@ -41,6 +64,15 @@ int main()
     head->link->link = current;
     
     printf("The number of nodes is: %d",count_of_nodes(head));
+    
+    struct node* target = head->link; 
+    insert_node(target, 18790);
+    printf("\n--------------\n");
+    printf("The number of nodes is: %d",count_of_nodes(head));
+    
+    struct node* target2 = traverse_nodes(head, 6); 
+    printf("\n--------------\n");
+    printf("%d",target2->data);
     
     return 0; 
 }
